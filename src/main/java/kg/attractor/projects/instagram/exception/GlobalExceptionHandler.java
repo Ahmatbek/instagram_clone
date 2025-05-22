f7910133734e327b5e2e1d75ad8a7f92385174ea
Ahmatbek <tursunbaevahmatbek@gmail.com>
2025-05-22 23:50:51 +0600

    hide certain buttons for unauthenticated user

--- src/main/java/kg/attractor/projects/instagram/exception/GlobalExceptionHandler.java
+++ src/main/java/kg/attractor/projects/instagram/exception/GlobalExceptionHandler.java
@@ -6,9 +6,12 @@ import kg.attractor.projects.instagram.service.ErrorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
@@ -35,4 +38,14 @@ public class GlobalExceptionHandler {
         model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
         return "errors/error";
     }
+
+    @ModelAttribute("user")
+    public void currentUser(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(!auth.getPrincipal().equals("anonymousUser")) {
+            model.addAttribute("authenticated", auth.getName());
+        }
+
+    }
+
 }
